93aa44db7ae4730bb82043628ea7bd04de7a861d
iwase22334 <iwase22334@gmail.com>
2017-01-10 17:48:16 +0900

    Change space to use TAB

--- src/rs_controller.cpp
+++ src/rs_controller.cpp
@@ -27,42 +27,42 @@ int main(int argc, char **argv) try
     dev->enable_stream(rs::stream::depth, rs::preset::best_quality);
     try { dev->enable_stream(rs::stream::infrared2, rs::preset::best_quality); } catch(...) {}
 
-  	float depth_scale_meters = dev->get_depth_scale();
+    float depth_scale_meters = dev->get_depth_scale();
     rs::intrinsics z_intrinsic = dev->get_stream_intrinsics(rs::stream::depth);
     rs::extrinsics z_extrinsic = dev->get_extrinsics(rs::stream::depth, rs::stream::color);
-	
-	ImagePublisher ip_depth(sizeof(uint16_t), "rs_camera/image_depth", "mono16");
-	ImagePublisher ip_color(sizeof(unsigned char) * 3, "rs_camera/image_color", "rgb8");
-	ImagePublisher ip_color_aligned_to_depth(sizeof(unsigned char) * 3, "rs_camera/image_color_aligned_to_depth", "rgb8");
+    
+    ImagePublisher ip_depth(sizeof(uint16_t), "rs_camera/image_depth", "mono16");
+    ImagePublisher ip_color(sizeof(unsigned char) * 3, "rs_camera/image_color", "rgb8");
+    ImagePublisher ip_color_aligned_to_depth(sizeof(unsigned char) * 3, "rs_camera/image_color_aligned_to_depth", "rgb8");
     ImagePublisher ip_depth_aligned_to_color(sizeof(unsigned char) * 3, "rs_camera/image_depth_aligned_to_color", "mono16");
 
     DeprojectService deproject_service(depth_scale_meters, z_intrinsic, z_extrinsic, &ip_depth);
 
-	// start all service
+    // start all service
     dev->start();
-	deproject_service.start_service();
-	ip_depth.init();
-	ip_color.init();
-	ip_color_aligned_to_depth.init();
+    deproject_service.start_service();
+    ip_depth.init();
+    ip_color.init();
+    ip_color_aligned_to_depth.init();
     ip_depth_aligned_to_color.init();
 
     // publish functions
-	std::function<void(int, int, const void*)> depth_publish = std::bind(static_cast<void (ImagePublisher::*)(int, int, const void*)>
-	   (&ImagePublisher::publish), &ip_depth, 
-		std::placeholders::_1, 
-		std::placeholders::_2, 
-		std::placeholders::_3);
-	std::function<void(int, int, const void*)> color_publish = std::bind(static_cast<void (ImagePublisher::*)(int, int, const void*)>
-		(&ImagePublisher::publish), &ip_color, 
-		std::placeholders::_1, 
-		std::placeholders::_2, 
-		std::placeholders::_3);
-	std::function<void(int, int, const void*)> color_aligned_to_depth_publish = std::bind(static_cast<void (ImagePublisher::*)(int, int, const void*)>
-		(&ImagePublisher::publish), 
-		&ip_color_aligned_to_depth, 
-		std::placeholders::_1, 
-		std::placeholders::_2, 
-		std::placeholders::_3);
+    std::function<void(int, int, const void*)> depth_publish = std::bind(static_cast<void (ImagePublisher::*)(int, int, const void*)>
+       (&ImagePublisher::publish), &ip_depth, 
+        std::placeholders::_1, 
+        std::placeholders::_2, 
+        std::placeholders::_3);
+    std::function<void(int, int, const void*)> color_publish = std::bind(static_cast<void (ImagePublisher::*)(int, int, const void*)>
+        (&ImagePublisher::publish), &ip_color, 
+        std::placeholders::_1, 
+        std::placeholders::_2, 
+        std::placeholders::_3);
+    std::function<void(int, int, const void*)> color_aligned_to_depth_publish = std::bind(static_cast<void (ImagePublisher::*)(int, int, const void*)>
+        (&ImagePublisher::publish), 
+        &ip_color_aligned_to_depth, 
+        std::placeholders::_1, 
+        std::placeholders::_2, 
+        std::placeholders::_3);
     std::function<void(int, int, const void*)> depth_aligned_to_color_publish = std::bind(static_cast<void (ImagePublisher::*)(int, int, const void*)>
         (&ImagePublisher::publish), 
         &ip_depth_aligned_to_color, 
@@ -73,24 +73,24 @@ int main(int argc, char **argv) try
     ros::Rate loop_rate(10);
     while(n.ok())
     {
-    	dev->wait_for_frames();
-    	
-    	depth_publish(dev->get_stream_height(rs::stream::depth), 
-    								   dev->get_stream_width(rs::stream::depth), 
-    								   dev->get_frame_data(rs::stream::depth));
-		
-    	color_publish(dev->get_stream_height(rs::stream::color), 
-    								   dev->get_stream_width(rs::stream::color), 
-    								   dev->get_frame_data(rs::stream::color));
-    	
-    	color_aligned_to_depth_publish(dev->get_stream_height(rs::stream::color_aligned_to_depth), 
-    								   dev->get_stream_width(rs::stream::color_aligned_to_depth), 
-    								   dev->get_frame_data(rs::stream::color_aligned_to_depth));
-		
-    depth_aligned_to_color_publish(dev->get_stream_height(rs::stream::depth_aligned_to_color), 
-       dev->get_stream_width(rs::stream::depth_aligned_to_color), 
-       dev->get_frame_data(rs::stream::depth_aligned_to_color));
-    	loop_rate.sleep();
+        dev->wait_for_frames();
+        
+        depth_publish(dev->get_stream_height(rs::stream::depth), 
+                                       dev->get_stream_width(rs::stream::depth), 
+                                       dev->get_frame_data(rs::stream::depth));
+        
+        color_publish(dev->get_stream_height(rs::stream::color), 
+                                       dev->get_stream_width(rs::stream::color), 
+                                       dev->get_frame_data(rs::stream::color));
+        
+        color_aligned_to_depth_publish(dev->get_stream_height(rs::stream::color_aligned_to_depth), 
+                                       dev->get_stream_width(rs::stream::color_aligned_to_depth), 
+                                       dev->get_frame_data(rs::stream::color_aligned_to_depth));
+        
+        depth_aligned_to_color_publish(dev->get_stream_height(rs::stream::depth_aligned_to_color), 
+                                        dev->get_stream_width(rs::stream::depth_aligned_to_color), 
+                                        dev->get_frame_data(rs::stream::depth_aligned_to_color));
+        loop_rate.sleep();
     }
     dev->stop();
 
